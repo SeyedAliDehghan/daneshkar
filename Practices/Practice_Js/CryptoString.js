9b64186a8f766ea3e15d82a54e661b312db3dec0
Seyed Ali Dehghan <alizzz6006@gmail.com>
2021-10-30 20:50:11 +0330

    js string crypt fixed

--- Practices/Practice_Js/CryptoString.js
+++ Practices/Practice_Js/CryptoString.js
@@ -2,27 +2,28 @@ function crypt(string){
     let alphabet = 'abcdefghijklmnopqrstuvwxyz';
     let alphabetArr = Array.from(String(alphabet));
     let strArr = Array.from(String(string));
-    // console.log(strArr);
-    strArr.unshift(strArr.pop());
-    console.log(strArr);
     for (let i=0;i<strArr.length;i++){
         let tmpIndex=alphabetArr.indexOf(strArr[i]);
-        console.log(strArr[i]);
         strArr[i]=alphabetArr[tmpIndex+1];
-        // console.log(strArr);
 
     }
     let done=strArr.join('');
-    console.log(done)
     return done;
 }
 function run(num,string){
     let tmp=string;
     for (let i=0;i<num;i++){
-        tmp=crypt(tmp);
+        tmp=reverse(tmp);
+        tmp=crypt(tmp)
     }
-    console.log(tmp);
+    return tmp;
+}
+function reverse(str){
+    let strArr = Array.from(String(str));
+    strArr.unshift(strArr.pop());
+    return strArr.join('');
 }
 
-let str="abc";
-run(2,str)
+let str="abcd";
+let cryptedStr=run(5,str);
+console.log(cryptedStr);
