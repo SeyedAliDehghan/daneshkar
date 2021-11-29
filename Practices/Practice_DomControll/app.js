b5463098ab77d673c094a010e4bf442f13538b16
Seyed Ali Dehghan <alizzz6006@gmail.com>
2021-11-29 20:14:11 +0330

    a little change

--- Practices/Practice_DomControll/app.js
+++ Practices/Practice_DomControll/app.js
@@ -14,13 +14,27 @@ function add(event) {
   listItem.appendChild(span);
   const removeBtn = document.createElement("button");
   removeBtn.innerText="remove";
-  removeBtn.classList.add("romove");
+  removeBtn.classList.add("remove");
   listItem.appendChild(removeBtn);
+  const updateBtn = document.createElement("button");
+  updateBtn.innerText="update";
+  updateBtn.classList.add("update");
+  listItem.appendChild(updateBtn);
   list.appendChild(listItem);
 }
 function remove(event) {
     const item = event.target;
     // item.parentElement=remove();
-    console.log(item.parentElement);
-    item.parentElement.remove();
+    //console.log(item.parentElement);
+    // console.log();
+  //   item.parentElement.remove();
+    if (item.classList[0]==='remove') {
+      item.parentElement.remove();
+  }else if(item.classList[0]==='update'){
+    // window.prompt(item.parentElement)
+    const li=item.parentElement;
+    const newText=window.prompt("enter new:",li.children[0].innerText);
+    li.children[0].innerText=newText;
+    // console.log(newText);
+  }
 }
--- Practices/Practice_addToList(array)/app.js
+++ Practices/Practice_addToList(array)/app.js
@@ -1,34 +1,59 @@
-let arr=[];
+let arr = [];
 
 button = document.querySelector("button");
 input = document.querySelector("input");
 list = document.querySelector(".list");
 
-
 button.addEventListener("click", add);
-list.addEventListener('click',remove)
+list.addEventListener("click", remove);
 
 function add(event) {
   event.preventDefault();
   arr.push(input.value);
   render();
 }
+
 function remove(event) {
-    const item = event.target;
-    // item.parentElement=remove();
-    console.log(item.parentElement);
-    item.parentElement.remove();
+  const item = event.target;
+  // item.parentElement=remove();
+  //console.log(item.parentElement);
+  // console.log();
+  //   item.parentElement.remove();
+  const li = item.parentElement;
+
+  if (item.classList[0] === "remove") {
+    arr = arr.filter((item) => item !== li.children[0].innerText);
+    render();
+    // item.parentElement.remove();
+  } else if (item.classList[0] === "update") {
+    // window.prompt(item.parentElement)
+    const li = item.parentElement;
+    const newText = window.prompt("enter new:", li.children[0].innerText);
+    arr=arr.map((item)=> item === li.children[0].innerText?newText:item);
+    render();
+
+    // li.children[0].innerText = newText;
+    // console.log(newText);
+  }
 }
 function render() {
-  for(let i=0;i<arr.length;i++){
-  const listItem = document.createElement("li");
-  const span=document.createElement("span");
-  span.innerText=arr[i];
-  listItem.appendChild(span);
-  const removeBtn = document.createElement("button");
-  removeBtn.innerText="remove";
-  removeBtn.classList.add("romove");
-  listItem.appendChild(removeBtn);
-  list.appendChild(listItem);
+  list.innerText = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    const listItem = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = arr[i];
+    listItem.appendChild(span);
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "remove";
+    removeBtn.classList.add("remove");
+    listItem.appendChild(removeBtn);
+    const updateBtn = document.createElement("button");
+    updateBtn.innerText = "update";
+    updateBtn.classList.add("update");
+    listItem.appendChild(updateBtn);
+    list.appendChild(listItem);
   }
+  console.log(arr);
 }
